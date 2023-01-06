20708c5e0569b97644ab3b07e0a3acaf7ef97446
eramazanoglu <eramazanoglu@magiclick.com>
2023-01-06 13:37:03 +0300

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,91 +1,65 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
+import React, { useEffect, useState } from "react";
+import { images } from "../utils.js";
 
-const inter = Inter({ subsets: ['latin'] })
+const page = () => {
+  const [gameCard, setGameCard] = useState([]);
+  const [disabled, setDisabled] = useState(false);
 
-export default function Home() {
+  useEffect(() => {
+    setGameCard(images.sort(() => Math.random() - 0.5));
+  }, []);
+
+  const [selectItem, setSelectItem] = useState(-1);
+
+  const selectCard = (id) => {
+    if (selectItem === -1) {
+      gameCard[id].select = "active";
+      setGameCard([...gameCard]);
+      setSelectItem(id);
+    } else {
+      if (gameCard[id].id == gameCard[selectItem].id) {
+        gameCard[id].select = "success";
+        gameCard[selectItem].select = "success";
+        setGameCard([...gameCard]);
+        setSelectItem(-1);
+      } else {
+        gameCard[selectItem].select = "";
+        gameCard[id].select = "wrong";
+        gameCard[selectItem].select = "wrong";
+        setDisabled(true);
+        setGameCard([...gameCard]);
+        setTimeout(() => {
+          gameCard[id].select = "";
+          gameCard[selectItem].select = "";
+          setGameCard([...gameCard]);
+          setDisabled(false);
+          setSelectItem(-1);
+        }, 1000);
+      }
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.jsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="app-content">
+      <div className="content">
+        <div className="title ">Memory Game</div>
+        <div className="game">
+          {gameCard.map((item, key) => (
+            <button
+              disabled={disabled}
+              className={`card  card-${item.select}`}
+              onClick={() => selectCard(key)}
+              id={item.id}
+              key={key}
+            >
+              <img src={item.image} />
+            </button>
+          ))}
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default page;
---
+++ utils.js/index.js
@@ -0,0 +1,84 @@
+const images = [
+  {
+    id: 1,
+    image: "img/angular.png",
+    select: null
+  },
+  {
+    id: 1,
+    image: "img/angular.png",
+    select: null
+  },
+  {
+    id: 2,
+    image: "img/css.png",
+    select: null
+  },
+  {
+    id: 2,
+    image: "img/css.png",
+    select: null
+  },
+  {
+    id: 3,
+    image: "img/html.png",
+    select: null
+  },
+  {
+    id: 3,
+    image: "img/html.png",
+    select: null
+  },
+  {
+    id: 4,
+    image: "img/js.png",
+    select: null
+  },
+  {
+    id: 4,
+    image: "img/js.png",
+    select: null
+  },
+  {
+    id: 5,
+    image: "img/nodejs.png",
+    select: null
+  },
+  {
+    id: 5,
+    image: "img/nodejs.png",
+    select: null
+  },
+  {
+    id: 6,
+    image: "img/react.png",
+    select: null
+  },
+  {
+    id: 6,
+    image: "img/react.png",
+    select: null
+  },
+  {
+    id: 7,
+    image: "img/scss.png",
+    select: null
+  },
+  {
+    id: 7,
+    image: "img/scss.png",
+    select: null
+  },
+  {
+    id: 8,
+    image: "img/vue.png",
+    select: null
+  },
+  {
+    id: 8,
+    image: "img/vue.png",
+    select: null
+  }
+];
+
+export { images };
